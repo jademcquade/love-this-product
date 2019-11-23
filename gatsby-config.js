93f04e7d78cbfe5fcb395366822c5750928416ae
Jordan McQuade <jordan@Jordans-MacBook-Pro.local>
2019-11-23 20:47:29 +0000

    change theme color to yellow

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
         short_name: `Love This Product`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#6b6b6b`,
+        theme_color: `#fed139`,
         display: `minimal-ui`,
         icon: `content/assets/logo.png`,
       },
